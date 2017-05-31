d72259a3ab2b4f35b15720ccdca410ec287b16aa
Deny Prasetyo <jasoet87@gmail.com>
2017-05-31 14:36:44 +0700

    This is not a Module

--- src/main/kotlin/org/diskursus/module/UserRepositoryImpl.kt
+++ src/main/kotlin/org/diskursus/module/UserRepositoryImpl.kt
@@ -1,17 +1,12 @@
 package org.diskursus.module
 
-import dagger.Module
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.mongo.MongoClient
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
-import io.vertx.rx.java.RxHelper
-import org.diskursus.DiskursusConfiguration
-import org.diskursus.ext.observable
 import org.diskursus.ext.single
 import org.diskursus.model.User
 import org.diskursus.repository.UserRepository
-import rx.Observable
 import rx.Single
 import javax.inject.Inject
 
@@ -20,14 +15,14 @@ import javax.inject.Inject
  *
  * @author Alex Xandra Albert Sim
  */
-@Module
-class UserModule @Inject constructor(val client: MongoClient): UserRepository {
-    val DocName = "users"
+
+class UserRepositoryImpl @Inject constructor(val client: MongoClient) : UserRepository {
+    val docName = "users"
 
     override fun getAllUsers(): Single<List<User>> {
-        val result = single<List<JsonObject>> { client.find(DocName, json { obj() }, it) }
+        val result = single<List<JsonObject>> { client.find(docName, json { obj() }, it) }
 
-        return result.map{ r -> r.map{ u -> User.fromJson(u) } }
+        return result.map { r -> r.map { u -> User.fromJson(u) } }
     }
 
     override fun getUsers(page: Int, perPage: Int): List<User> {
